107f463d005280f1c74697d49ccc6cd70678e04c
bumju08 <debugprov@naver.com>
2022-11-09 15:34:09 +0900

    Pandas Dataframe으로부터 데이터를 정리하는 모듈

---
+++ miner.py
@@ -0,0 +1,22 @@
+import os, pandas, pandas.core.frame
+import reader
+
+
+class dfMiner:
+    def __init__(self):
+        pass
+
+    def getPopulation(self) -> dict:
+        cReader = reader.csvReader()
+        dataFrame = cReader.CsvToDataframe("popluation")
+
+
+
+
+
+
+
+        
+
+
+    
