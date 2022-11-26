8fcfc7711aa1d259cf67a74684f0c2e7085966b5
bumju08 <debugprov@naver.com>
2022-11-27 04:26:03 +0900

    Implement configurator.py to generate seoul area code

---
+++ configurator.py
@@ -0,0 +1,78 @@
+import os, json, re
+import miner, reader
+
+
+
+class areaCode:
+    def __init__(self):
+        self.popMiner = miner.population()
+        self.dir = './db'
+        self.fileName = 'area_code.json'
+
+    # Private Function
+    # population csv 파일로부터 각 자치구별 인구수를 반환하는 함수
+    def __getSeoul(self) -> dict:
+        areaData = self.popMiner.fromSeoul()
+
+        if areaData == None:
+            return None
+
+        areaData.pop('소계')
+
+        if not os.path.isdir(self.dir):
+            os.mkdir(self.dir)
+
+        for index, key in enumerate(list(areaData.keys())):
+            areaData[index + 1] = key
+            areaData.pop(key)
+
+        return areaData
+
+    # 내부적으로 자치구별 인구수를 추출하여 json 파일로 쓰는 함수
+    def writeSeoul(self) -> None:
+        areaData = self.__getSeoul()
+
+        with open(self.dir + '/' + self.fileName, 'w', encoding='utf-8') as json_file:
+            json.dump(areaData, json_file, indent=4, ensure_ascii=False)
+    
+    # 자치구별 인구수를 반환하는 함수
+    def readSeoul(self) -> dict:
+        if not os.path.isfile(self.dir + '/' + self.fileName):
+            return self.__getSeoul()
+
+        areaData = dict()
+        with open(self.dir + '/' + self.fileName, 'r+', encoding='utf-8') as json_file:
+            areaData = json.load(json_file)
+
+        for key, value in areaData.items():
+            if not (str(key).isdigit() or str(value).endswith('구')):
+                return self.__getSeoul()
+
+        return areaData
+        
+
+
+class subwayStation:
+    def __init__(self):
+        self.dfReader = reader.dataframe()
+
+    def readSeoul(self) -> dict:
+        dataframe = self.dfReader.fromCsv("subwayarea")
+        if dataframe is None:
+            return None
+
+        areas = dataframe['자치구'].values
+        raw_stations = dataframe['해당역(호선)'].values
+        area_stations = {}
+
+        for index, stations in enumerate(raw_stations):
+            # 정규 표현식을 이용해 "용마산(7), 망우(중앙)"와 같은 문자열을 "용마산, 망우"로 치환하여 불필요한 호선명 제거
+            pattern = r"\([0-9A-Za-z가-힣]+\)"
+            stations = re.sub(pattern, "", stations)
+
+            # 자치구별로 모든 역 저장
+            area_stations[areas[index]] = stations.split(", ")
+
+        return area_stations
+
+
